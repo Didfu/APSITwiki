6c3fe5171343110b25ee21fd7b07bea68746986d
dhruvmahyavanshi <mahyavanshidhruv@gmail.com>
2024-09-29 16:23:55 +0530

    updates

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,12 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index", views.index, name="index"),
     path("wiki/<str:title>", views.entryPage, name="entrypage"),
     path("search", views.search, name="search"),
     path("new", views.createEntry, name="newentry"),
     path("edit", views.editEntry, name="edit"),
     path("random", views.randomPage, name="random"),
     path("Function", views.Function, name="Function"),
-    path("", views.user, name="login")
+    path("", views.index, name="index")
 ]
